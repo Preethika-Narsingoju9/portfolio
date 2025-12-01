eccb2b28a703e9e2e795eea3415e0b9272668f43
Preethika Narsingoju <navyaasuresh009@gmail.com>
2025-12-01 10:32:08 +0530

    Initial portfolio with Java projects

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+});
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault(); alert('Thank you for your message! (Demo form)');
+});
