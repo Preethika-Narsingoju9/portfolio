82bf6725d2f344df4f734f768ce547c44ca39fce
Preethika Narsingoju <navyaasuresh009@gmail.com>
2026-06-03 21:38:58 +0530

    Updated portfolio design

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-//     anchor.addEventListener('click', function (e) {
-//         e.preventDefault(); document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
-//     });
-// });
-// document.getElementById('contactForm').addEventListener('submit', function(e) {
-//     e.preventDefault(); alert('Thank you for your message! (Demo form)');
-// });
-
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
